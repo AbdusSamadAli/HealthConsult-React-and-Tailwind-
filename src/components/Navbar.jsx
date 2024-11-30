a4de615593c7df7d78dfad9857b02b21ee76d54f
AbdusSamadAli <samad140804@gmail.com>
2024-11-30 21:13:48 +0530

    changes made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,78 +1,31 @@
-import { Link } from "react-router-dom";
-import { FaUserMd } from "react-icons/fa"; 
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaUserDoctor } from "react-icons/fa6";
 const Navbar = () => {
   return (
     <div className="bg-blue-50">
       <header className="bg-gradient-to-r from-blue-200 to-blue-400 p-6 shadow-lg">
         <div className="container mx-auto flex items-center justify-between">
           <div className="flex items-center">
-            <FaUserMd className="text-3xl text-white mr-2" />
+            <FaUserDoctor className="text-3xl text-white mr-2" />
             <Link to="/" className="text-3xl font-bold text-white">
               HealthConsult
             </Link>
           </div>
-          <nav className="w-full md:flex items-center justify-between space-x-4 hidden md:flex">
-            <Link
-              to="/"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
+          <nav className="flex items-center">
+            <Link to="/" className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300 mr-4">
               Home
             </Link>
-            <Link
-              to="/doctors"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
+            <Link to="/doctors" className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300 mr-4">
               Doctors
             </Link>
-            <Link
-              to="/dashboard"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
+            <Link to="/dashboard" className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300 mr-4">
               Dashboard
             </Link>
-            <Link
-              to="/book-appointment"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
+            <Link to="/book-appointment" className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300 mr-4">
               Book Appointment
             </Link>
-            <Link
-              to="/sign-in"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
-              Sign In
-            </Link>
-          </nav>
-          <nav className="md:hidden w-full flex flex-col space-y-4 mt-4">
-            <Link
-              to="/"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
-              Home
-            </Link>
-            <Link
-              to="/doctors"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
-              Doctors
-            </Link>
-            <Link
-              to="/dashboard"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
-              Dashboard
-            </Link>
-            <Link
-              to="/book-appointment"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
-              Book Appointment
-            </Link>
-            <Link
-              to="/sign-in"
-              className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300"
-            >
+            <Link to="/sign-in" className="text-lg text-white hover:text-blue-600 hover:underline transition-colors duration-300">
               Sign In
             </Link>
           </nav>
@@ -83,6 +36,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
 
 
