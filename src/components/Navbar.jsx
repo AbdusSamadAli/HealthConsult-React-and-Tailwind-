834693f65e0fab9da9b3c18a6d7df20e9fb5d993
AbdusSamadAli <samad140804@gmail.com>
2024-11-30 21:04:18 +0530

    changes made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,17 @@
 import { Link } from "react-router-dom";
-import { FaUserMd } from "react-icons/fa"; // Corrected import
+import { FaUserMd } from "react-icons/fa"; 
 
 const Navbar = () => {
   return (
     <div className="bg-blue-50">
       <header className="bg-gradient-to-r from-blue-200 to-blue-400 p-6 shadow-lg">
         <div className="container mx-auto flex items-center justify-between">
-          {/* Logo Section */}
           <div className="flex items-center">
-            <FaUserMd className="text-3xl text-white mr-2" /> {/* Updated icon */}
+            <FaUserMd className="text-3xl text-white mr-2" /> 
             <Link to="/" className="text-3xl font-bold text-white">
               HealthConsult
             </Link>
           </div>
-
-          {/* Navigation Links */}
           <nav className="hidden md:flex items-center">
             <Link
               to="/"
@@ -47,14 +44,6 @@ const Navbar = () => {
               Sign In
             </Link>
           </nav>
-
-          {/* Mobile menu (hamburger icon) */}
-          <div className="md:hidden flex items-center">
-            <button className="text-white text-3xl">
-              {/* Hamburger icon */}
-              ☰
-            </button>
-          </div>
         </div>
       </header>
     </div>
